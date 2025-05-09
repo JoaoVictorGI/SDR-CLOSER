8828bec9ada33877373c27f2e01c2ac1d7cb5227
João Victor <joaovictor.gizidoro@gmail.com>
2025-05-09 13:52:54 -0300

    feat: create method to find by id_atendimento

--- src/main/java/com/server/vendas/server_vendas/historicoatendimento/HistoricoAtendimentoRepository.java
+++ src/main/java/com/server/vendas/server_vendas/historicoatendimento/HistoricoAtendimentoRepository.java
@@ -1,9 +1,13 @@
 package com.server.vendas.server_vendas.historicoatendimento;
 
+import com.server.vendas.server_vendas.atendimento.AtendimentoModel;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HistoricoAtendimentoRepository
-    extends JpaRepository<HistoricoAtendimentoModel, UUID> {}
+    extends JpaRepository<HistoricoAtendimentoModel, UUID> {
+  Optional<HistoricoAtendimentoModel> findByIdAtendimento(AtendimentoModel idAtendimento);
+}
